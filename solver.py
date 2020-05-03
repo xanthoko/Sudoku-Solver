5ebd9c4366c19418b8d83d78d7a790ae0ddec0f3
Kostas Xanthopoulos <konxantho@gmail.com>
2020-05-03 03:03:43 +0300

    working sudoku solver

--- solver.py
+++ solver.py
@@ -19,21 +19,80 @@ def get_legal_values(sudoku, row, col):
     # restricted values
     row_values = list(sudoku[row, :])
     col_values = list(sudoku[:, col])
-    # the set of values that are not availabe
-    restricted_set = set()
-    [restricted_set.add(x) for x in row_values + col_values]
-    # available values = total - restricted
-    availabe_values = [x for x in total_range if x not in restricted_set]
 
-    # TODO: check the square restriction
+    square_r_index = row // 3
+    square_c_index = col // 3
+    square_r_range = (square_r_index * 3, (square_r_index + 1) * 3)
+    square_c_range = (square_c_index * 3, (square_c_index + 1) * 3)
+    sqaure = sudoku[square_r_range[0]:square_r_range[1], square_c_range[0]:
+                    square_c_range[1]]
+    flat_square = list(sqaure.flatten())
+
+    # the set of values that are not availabe
+    restricted_values = list(set(row_values + col_values + flat_square))
+    # if row == 0 and col == 6:
+    #     print(sudoku)
+    #     print(restricted_values)
+
+    # available values = total - restricted
+    availabe_values = [x for x in total_range if x not in restricted_values]
 
     return availabe_values
+
+
+def recursion_solve(sudoku, r_ind, c_ind):
+    # print(r_ind, c_ind)
+    if sudoku[r_ind, c_ind]:
+        next_c = c_ind
+        next_r = r_ind
+        if c_ind < 8:
+            next_c += 1
+        else:
+            next_r += 1
+            next_c = 0
+
+        # recursion call
+        ret = recursion_solve(sudoku, next_r, next_c)
+        # already filled
+        return ret
+
+    availabe_values = get_legal_values(sudoku, r_ind, c_ind)
+    # if r_ind == 8 and c_ind == 8:
+    #     print(availabe_values)
+
+    if not availabe_values:
+        return None
+
+    fake_sudoku = sudoku.copy()
+    for value in availabe_values:
+        # fill the current position
+        fake_sudoku[r_ind, c_ind] = value
+        # get the next position
+        next_c = c_ind
+        next_r = r_ind
+        if c_ind < 8:
+            next_c += 1
+        else:
+            next_r += 1
+            next_c = 0
+
+        if next_r > 8 or next_c > 8:
+            return fake_sudoku
+
+        # recursion call
+        ret = recursion_solve(fake_sudoku, next_r, next_c)
+
+        if ret is None:
+            continue
+        else:
+            return ret
 
 
 def solve():
     sudoku = get_sudoku_array()
 
     history = OrderedDict()
+    counter = 0
 
     r_ind = 0
     c_ind = 0
@@ -41,7 +100,11 @@ def solve():
         sudoku_value = sudoku[r_ind, c_ind]
 
         if not sudoku_value:
-            availabe_values = get_legal_values(sudoku, r_ind, c_ind)
+            try:
+                availabe_values = history[(r_ind, c_ind)]
+            except KeyError:
+                # KeyError: newly visited tile
+                availabe_values = get_legal_values(sudoku, r_ind, c_ind)
 
             if availabe_values:
                 # fill the tile
@@ -56,10 +119,18 @@ def solve():
                 # erase the filled value, NOTE: is this neccessary?
                 sudoku[latest_filled] = 0
                 latest_available = history[latest_filled]
-                print(sudoku)
-                print(latest_available)
-                # continue
-                break
+
+                if not latest_available:
+                    del history[latest_filled]
+                    latest_filled = next(reversed(history))
+
+                r_ind, c_ind = latest_filled
+                counter += 1
+                if counter == 9:
+                    print(r_ind, c_ind)
+                    print(sudoku)
+                    break
+                continue
 
         if c_ind < 8:
             c_ind += 1
@@ -75,4 +146,7 @@ def solve():
     print(history)
 
 
-solve()
+# solve()
+sudoku = get_sudoku_array()
+d = recursion_solve(sudoku, 0, 0)
+print(d)
